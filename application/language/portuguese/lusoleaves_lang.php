666a17b5f47715ee870ed36abd3561158f580fb5
harokb <harokb@gmail.com>
2013-11-06 20:36:49 +0000

    Updated portuguese text.

--- application/language/portuguese/lusoleaves_lang.php
+++ application/language/portuguese/lusoleaves_lang.php
@@ -8,16 +8,16 @@ $lang['register'] = 'Registo';
 
 $lang['companyDescription1'] = '
 Rogil faz parte do concelho de Aljezur, situado perto do oceano Atlântico e permite uma produção contínua em boas condições.
-A região beneficia de poucos dias de frio no Inverno e temperaturas amenas durante o Verão . A empresa está situada entre outros
-grandes produtores de verduras decorativas, nomeadamente Luecodendron, leucospermum, proteas e bambus.';
+A região beneficia de poucos dias de frio no Inverno e temperaturas amenas durante o Verão. A empresa está situada entre
+outros grandes produtores de verduras decorativas, nomeadamente Luecodendron, leucospermum, proteas e bambus. ';
 
 $lang['companyDescription2'] = '
-A localidade do Rogil está bem localizada para exportações. Há transportadoras que vão para a Holanda e Inglaterra
-duas vezes por semana para entrega nos principais mercados. Temos 1,80 hectares de estufas de sombreamento,
-principalmente plantadas com fetos ornamentais e Ruscus. Estamos também a introduzir novos tipos de verduras, que ainda não são em produção.
+A localidade do Rogil está bem localizada para exportações. Há transportadoras que vão para a Holanda e Inglaterra duas
+vezes por semana para entrega nos principais mercados. Temos 1,80 hectares de estufas de sombreamento, principalmente
+plantadas com fetos ornamentais e Ruscus. Estamos também a introduzir novos tipos de verduras, que ainda não são em produção.
 Além das estufas de sombreamento, temos também estufas de plásticos, plantadas com aspidistras, Aralias, Phil Xanadu e Falcatum.';
 
-$lang['companyDescription3'] = 'Para qualquer informação adicional, envie-nos um email.';
+$lang['companyDescription3'] = 'Para qualquer informação adicional, envie-nos um email. ';
 
 
 $lang['findUsTitle'] = 'Onde estamos';
