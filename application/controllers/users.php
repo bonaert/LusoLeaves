3b707911b0717653441001935dc12f57a7aeb853
harokb <harokb@gmail.com>
2013-11-06 20:15:08 +0000

    Added is_admin variable

--- application/controllers/users.php
+++ application/controllers/users.php
@@ -28,6 +28,7 @@ class Users extends CI_CONTROLLER
     {
         $this->lang->load('lusoleaves');
         $data['is_logged_in'] = $this->session->userdata('is_logged_in');
+        $data['is_logged_in'] = $this->session->userdata('is_admin');
 
         $this->load->helper('form');
         $this->load->library('form_validation');
