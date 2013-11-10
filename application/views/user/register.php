a0e6def6618d7d0b381373eea7ceea27d658a689
harokb <harokb@gmail.com>
2013-11-10 13:30:36 +0000

    All user pages use template.

--- application/views/user/register.php
+++ application/views/user/register.php
@@ -1,78 +1,3 @@
-<!DOCTYPE html>
-<!--[if lt IE 7]>
-<html class="lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
-<!--[if IE 7]>
-<html class="lt-ie9 lt-ie8" lang="en"> <![endif]-->
-<!--[if IE 8]>
-<html class="lt-ie9" lang="en"> <![endif]-->
-<!--[if gt IE 8]><!-->
-<html lang="en"> <!--<![endif]-->
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <title>Luso Leaves - Register</title>
-    <link type="text/css" rel="stylesheet" href="<?php echo asset_url() ?>/css/form.css">
-    <link type="text/css" rel="stylesheet" href="<?php echo asset_url() ?>/css/default.css">
-
-    <script src="<?php echo asset_url() ?>/js/sha512.js"></script>
-    <script src="<?php echo asset_url() ?>/js/forms.js"></script>
-
-</head>
-<body>
-<div class="icons">
-    <a href="<?= base_url() . $this->lang->switch_uri('pt'); ?>">
-        <img src="https://cdn1.iconfinder.com/data/icons/flags_gosquared/48/Portugal_flat.png" alt="Portuguese"/>
-    </a>
-
-    <a href="<?= base_url() . $this->lang->switch_uri('en'); ?>">
-        <img src="https://cdn1.iconfinder.com/data/icons/flags/flags/48/United%20Kingdom%28Great%20Britain%29.png"
-             alt="English"/>
-    </a>
-</div>
-<div id="menu">
-    <ul>
-        <li>
-            <a href="<?= site_url('main') ?>" accesskey="1" title="">
-                <?php echo $this->lang->line("homepage"); ?>
-            </a>
-        </li>
-        <li>
-            <a href="<?= site_url('products') ?>" accesskey="2" title="">
-                <?php echo $this->lang->line("products"); ?>
-            </a>
-        </li>
-
-        <?php if ($is_admin): ?>
-            <li>
-                <a href="<?= site_url('users') ?>" accesskey="2" title="">
-                    Users
-                </a>
-            </li>
-        <?php endif; ?>
-
-        <?php if ($is_logged_in): ?>
-            <li>
-                <a href="<?= site_url('users/logout') ?>" accesskey="3" title="">
-                    <?php echo $this->lang->line("logout"); ?>
-                </a>
-            </li>
-        <?php else: ?>
-            <li>
-                <a href="<?= site_url('users/login') ?>" accesskey="3" title="">
-                    <?php echo $this->lang->line("login"); ?>
-                </a>
-            </li>
-        <?php endif; ?>
-
-
-        <li>
-            <a href="<?= site_url('users/register') ?>" accesskey="4" title="">
-                <?php echo $this->lang->line("register"); ?>
-            </a>
-        </li>
-    </ul>
-</div>
-
 <div class="login">
     <h1>
         <?= lang('register') ?>
@@ -127,7 +52,4 @@
         </p>
     </form>
 </div>
-
-</body>
-</html>
 
