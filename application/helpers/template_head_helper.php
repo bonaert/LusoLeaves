7be99c338d7d79ebd4b823caf63cc636b88b6083
harokb <harokb@gmail.com>
2013-11-10 12:30:42 +0000

    Changed base url.

--- application/helpers/template_head_helper.php
+++ application/helpers/template_head_helper.php
@@ -5,7 +5,7 @@ if (!function_exists('format_css')) {
     function format_css($css_file)
     {
         $decl = '<link rel="stylesheet" type="text/css" href="%s%s" />';
-        $base = asset_url() . "css/";
+        $base = base_url() . 'assets/css/';
         $css = '';
         foreach ($css_file as $item) {
             if (!empty($item)) {
@@ -21,7 +21,7 @@ if (!function_exists('format_js')) {
     function format_js($js_file)
     {
         $decl = '<script type="text/javascript" src="%s%S"></script>';
-        $base = asset_url() . "js/";
+        $base = base_url() . 'assets/js/';
         $js = '';
         foreach ($js_file as $item) {
             if (!empty($item)) {
