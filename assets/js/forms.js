617f2cf29e1b904acd80862d82b5d3657f3d8687
Gerik Bonaert <dev@adaxisoft.be>
2013-11-11 13:47:11 +0100

    Do not create the element if it already exists. It might already exist if the user clicked once on the button but not all the required fields were filled in. Do not submit the form. This will be done by the submit button.

--- assets/js/forms.js
+++ assets/js/forms.js
@@ -1,11 +1,19 @@
 function formhash(form, password) {
-    // Create a new element input, this will be our hashed password field.
-    var p = document.createElement("input");
+    
+    var p = document.getElementById("CryptoJS");
+    if (!p) {
+    	// Create a new element input, this will be our hashed password field.
+    	p = document.createElement("input");
+        p.name = "hashedPassword";
+        p.type = "hidden";
+        p.id = "CryptoJS";
+    }
+    p.value = CryptoJS.SHA512(password.value);
+    
     // Add the new element to our form.
     form.appendChild(p);
-    p.name = "hashedPassword";
-    p.type = "hidden";
-    p.value = CryptoJS.SHA512(password.value);
+
+
 
     // Make sure the plaintext password doesn't get sent.
     // Need to put in some string garbage string, beca
@@ -19,7 +27,4 @@ function formhash(form, password) {
 
     password.value = repeat("a", password.value.length);
 
-
-    // Finally submit the form.
-    form.submit();
 }
