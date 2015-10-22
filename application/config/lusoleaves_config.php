f91ee740a5e4aa3bfc56a05b86c6b41ef2317e0a
adaxi <dev@adaxisoft.be>
2015-10-22 21:54:05 +0200

    Add non-sensitive configuration files

---
+++ application/config/lusoleaves_config.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| New user registration notification
+|--------------------------------------------------------------------------
+|
+| When a new user is added, a notification will be added to the manager of
+| the website. This field should contain an email address where this
+| notification is to be delivered.
+|
+*/
+$config['new_user_notification_email'] = 'info@lusoleaves.com';
+$config['noreply_email'] = 'noreply@lusoleaves.com';
