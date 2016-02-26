396615e502a13bc11d0f739801dc242c59d762fa
harokb <harokb@gmail.com>
2016-02-26 19:23:17 +0100

    Now considers different time intervals for the graph (reads them from the URL).

--- application/controllers/weather.php
+++ application/controllers/weather.php
@@ -16,8 +16,16 @@ class Weather extends CI_CONTROLLER {
 		$data ['_internal_js'] [] = 'Chart.Core.js';
 		$data ['_internal_js'] [] = 'Chart.Scatter.js';
 
+		$timeInterval = $this->input->get('timeInterval', TRUE);
+		$possibilities = array("1", "2", "7", "31");
+		if (in_array($timeInterval, $possibilities)){
+			$timeInterval = (int) $timeInterval;
+		} else {
+			$timeInterval = 1;
+		}
+
 		$data ['latest'] = $this->weathersnapshot_model->get_latest_weathersnapshot ();
-		$snapshots = $this->weathersnapshot_model->get_weather_snapshots(0,  1);
+		$snapshots = $this->weathersnapshot_model->get_weather_snapshots(0, $timeInterval);
 		$data ['snapshots'] = $snapshots;
 		$this->load->view ( 'template', $data );
 	}
