c29a1a2318bdcb26eaf2a422038a5842be737bbf
harokb <harokb@gmail.com>
2017-08-11 12:01:25 +0100

    Improved main page style

--- application/views/index.php
+++ application/views/index.php
@@ -1,40 +1,65 @@
 <div class="section-colored">
-	<div class="container">		
-		<div class="row">
-			<div class="col-lg-12">
-				<img src="<?php echo base_url() ?>/assets/images/logo.png" class="header-banner">
-				<h2>Luso Leaves</h2>
-				<p><?php echo $this->lang->line("companyDescription1"); ?></p>
-				<p><?php echo $this->lang->line("companyDescription2"); ?></p>
-				<p><?php echo $this->lang->line("companyDescription3"); ?></p>
-			</div>
-		</div>
-	</div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <img src="<?php echo base_url() ?>/assets/images/logo.png" class="img-responsive img-center">
+                <h2>Luso Leaves</h2>
+                <p><?php echo $this->lang->line("companyDescription1"); ?></p>
+                <p><?php echo $this->lang->line("companyDescription2"); ?></p>
+                <p><?php echo $this->lang->line("companyDescription3"); ?></p>
+            </div>
+        </div>
+    </div>
 </div>
 <div class="section">
-	<div class="container">
-		<div class="row">
-		<div class="col-lg-12">
-				<h2><?php echo $this->lang->line('findUsTitle'); ?></h2>  
-				<pre>
-<strong><?php echo $this->lang->line("findUsName"); ?></strong>: Lusoleaves Unipessoal Lda
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <h2><?php echo $this->lang->line('findUsTitle'); ?></h2>
 
-<strong><?php echo $this->lang->line("findUsAddress"); ?></strong>:  Estufa das Carapuças, Rogil, Aljezur, PORTUGAL
+                <ul class="list-group">
+                    <li class="list-group-item">
+                        <strong><?php echo $this->lang->line("findUsName"); ?></strong>: Lusoleaves Unipessoal Lda
+                    </li>
+                    <li class="list-group-item">
+                        <strong><?php echo $this->lang->line("findUsAddress"); ?></strong>: Estufa das Carapuças, Rogil,
+                        Aljezur, PORTUGAL
+                    </li>
+                    <li class="list-group-item">
+                        <strong><?php echo $this->lang->line("findUsPostalCode"); ?></strong>: Caixa Postal 363-L, 8670-440,
+                        Rogil, Aljezur, PORTUGAL
+                    </li>
+                    <li class="list-group-item">
+                        <strong>Email</strong>: <a href="mailto:info@lusoleavs.com">info@lusoleaves.com</a>
+                    </li>
+                    <li class="list-group-item">
+                        <strong><?php echo $this->lang->line("findUsPhoneNumber"); ?></strong>: (00351) 925 303 403
+                    </li>
+                    <li class="list-group-item">
+                        <strong><?php echo $this->lang->line("findUsCoordinates"); ?></strong>: 37.3666, -8.78535
+                    </li>
+                </ul>
 
-<strong><?php echo $this->lang->line("findUsPostalCode"); ?></strong>: Caixa Postal 363-L, 8670-440, Rogil, Aljezur, PORTUGAL
+<!--                <p><strong>--><?php //echo $this->lang->line("findUsName"); ?><!--</strong>: Lusoleaves Unipessoal Lda</p>-->
+<!---->
+<!--                <p><strong>--><?php //echo $this->lang->line("findUsAddress"); ?><!--</strong>: Estufa das Carapuças, Rogil,-->
+<!--                    Aljezur, PORTUGAL</p>-->
+<!---->
+<!--                <p><strong>--><?php //echo $this->lang->line("findUsPostalCode"); ?><!--</strong>: Caixa Postal 363-L, 8670-440,-->
+<!--                    Rogil, Aljezur, PORTUGAL</p>-->
+<!---->
+<!--                <p><strong>Email</strong>: <a href="mailto:info@lusoleavs.com">info@lusoleaves.com</a></p>-->
+<!---->
+<!--                <p><strong>--><?php //echo $this->lang->line("findUsPhoneNumber"); ?><!--</strong>: (00351) 925 303 403</p>-->
+<!---->
+<!--                <p><strong>--><?php //echo $this->lang->line("findUsCoordinates"); ?><!--</strong>: 37.3666, -8.78535</p>-->
 
-<strong>Email</strong>: <a href="mailto:info@lusoleavs.com">info@lusoleaves.com</a>
 
-<strong><?php echo $this->lang->line("findUsPhoneNumber"); ?></strong>: (00351) 925 303 403
-
-<strong><?php echo $this->lang->line("findUsCoordinates"); ?></strong>: 37.3666, -8.78535
-				</pre>
-
-				<!-- map goes here-->
-				<div id="map-canvas" class="map"></div>
-			</div>
-		</div>		
-	</div>
+                    <!-- map goes here-->
+                <div id="map-canvas" class="map"></div>
+            </div>
+        </div>
+    </div>
 </div>		
 
 
